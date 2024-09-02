abb0bb3a5b80042cb9ffe33c623a76444daf3353
MdShafique_Ziya <mdshafique.ziya@dellteam.com>
2024-09-02 10:53:09 +0530

    added project properties

--- Src/Microsoft.MongoDbBridge.Client/Extensions/ServiceExtenstions.cs
+++ Src/Microsoft.MongoDbBridge.Client/Extensions/ServiceExtenstions.cs
@@ -7,6 +7,14 @@ namespace Microsoft.MongoDbBridge.Client.Extensions
 {
 	public static class ServiceExtenstions
 	{
+		/// <summary>
+		/// Add the dependencies of the mongo serices
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="services"></param>
+		/// <param name="connString"></param>
+		/// <param name="database"></param>
+		/// <returns></returns>
 		public static IServiceCollection AddMongoService<T>(this IServiceCollection services,string connString,string database) where T : IEntity
 		{
 			services.AddSingleton(_ =>
